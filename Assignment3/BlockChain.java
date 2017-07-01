45a0b231565a29a170f19cfe72a9ddf648599121
Erik Rintala <adagiox@users.noreply.github.com>
2017-06-30 20:43:28 -0700

    Updated data structure to HashMap, compiles

--- Assignment3/BlockChain.java
+++ Assignment3/BlockChain.java
@@ -3,14 +3,18 @@
 // as it would cause a memory overflow.
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class BlockChain {
     // Block will contain a hash to the previous block
     
+    // Storing full blocks in the blockStore and updating blockStore on adding new blocks
+    // All blocks are stored by their hash in the blockChain
+    
     public static final int CUT_OFF_AGE = 10;
-    public Block maxHeightBlock; // Reference to the block at maxHeight
-    public ArrayList<Block> blockChain;
-    public BlockHandler blockHandler;
+    public ArrayList<ArrayList<Block>> blockStore; // Blocks
+    public HashMap<byte[], byte[]> blockChain; // HashMap Which is a map of the current blocks hash and value of the previous hash
+    public BlockHandler blockHandler;   //      i.e. HashMap<CurrentHash, PrevHash>;
     public TxHandler txHandler;
     public TransactionPool currentTXPool;
     public UTXOPool maxUTXOPool;
@@ -20,22 +24,25 @@ public class BlockChain {
      */
     public BlockChain(Block genesisBlock) {
         // IMPLEMENT THIS
-        // Set the pv block hash to null
-        blockChain = new ArrayList<Block>();
-        blockChain.add(genesisBlock);
-        maxHeightBlock = genesisBlock;
+       
+        blockChain = new HashMap<byte[], byte[]>();
+        blockStore = new ArrayList<ArrayList<Block>>();
+        blockHandler = new BlockHandler(this);
+        
+        // ** TODO ** Set up the genesis block correctly before adding
+        blockChain.put(genesisBlock.getHash(), null);
+        blockStore.add(new ArrayList<Block>());
+        blockStore.get(0).add(genesisBlock);
     }
 
     /** Get the maximum height block */
     public Block getMaxHeightBlock() {
-        // get all blocks by at maxHeight
-        // return the oldest if more than one
-        return maxHeightBlock;
+        // returns the oldest block at the max height of the blockchain
+        return blockStore.get(blockStore.size() - 1).get(0);
     }
 
     /** Get the UTXOPool for mining a new block on top of max height block */
     public UTXOPool getMaxHeightUTXOPool() {
-        // IMPLEMENT THIS
         // get the corresponding UTXOPool for the longest chain
         return maxUTXOPool;
     }
@@ -45,6 +52,32 @@ public class BlockChain {
         return currentTXPool;
     }
 
+    // Add a block to the blockstore
+    // if blockstore is larger than CUT_OFF_AGE, trim the bottom blocks
+    // block is valid
+    public boolean addBlockStore(Block block)
+    {
+        int height = 1;
+        for (ArrayList<Block> list : blockStore)
+        {
+            for (Block b : list)
+            {
+                if (b.getHash() == block.getPrevBlockHash())
+                {
+                    if (height == blockStore.size())
+                        blockStore.add(new ArrayList<Block>());
+                    ArrayList<Block> addB = blockStore.get(height);
+                    addB.add(block);
+                    if (blockStore.size() == 11)
+                        blockStore.remove(0);
+                    return true;
+                }
+                height++;
+            }
+        }
+        return false;
+    }
+    
     /**
      * Add {@code block} to the block chain if it is valid. For validity, all transactions should be
      * valid and block should be at {@code height > (maxHeight - CUT_OFF_AGE)}.
@@ -66,18 +99,19 @@ public class BlockChain {
         // Update maxHeight if necessary
         
         // Check for the height of the prev block hash in the blockchain up to depth of maxHeight - CUT_OFF_AGE
-        // I
-        if (block.getPrevBlockHash() == getMaxHeightBlock.getHash())
-        {
-            for (Transaction tx : block.getTransactions())
-            {
-                if (tx.isValidTx() == false)
-                    return false;
-            }
-            blockChain.add(block);
-            maxHeight++;
-            return true;
-        }
+        // Send block to process block to add coinbase tx and finalize
+        if (block.getPrevBlockHash() == null)
+            return false;
+        
+        // Validate TX
+        txHandler = new TxHandler(maxUTXOPool);
+        
+        // Add to blockStore
+        if (this.addBlockStore(block) != true)
+            return false;
+        
+        // Add to Block to HashMap
+        
         return false;
     }
 
